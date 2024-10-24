1e99f6e54e444ede754272f32fd6b0785550eeb5
Aleksia Loewen <aleksialoewen@uvic.ca>
2024-10-24 10:29:29 -0700

    aleksia init

--- src/components/terms-of-service.tsx
+++ src/components/terms-of-service.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+npmimport { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { ScrollArea } from "@/components/ui/scroll-area"
 
